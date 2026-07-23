4803be4acfee5e41fcf0935b8af515c1a9871a24
pswbuild <pswbuild@oppo.com>
2026-07-23 07:55:21 +0000

    Synchronize code for OnePlus CPH2745_16.0.9.400(EX01) CPH2747_16.0.9.400(EX01) CPH2749_16.0.9.400(EX01) PLK110_16.0.9.400(CN01) Based on QCOM release TAG:AU_LINUX_KERNEL.PLATFORM.5.0.R1.00.00.00.099.064
    Adapted for vendor/qcom/kernel on lineage-23.2. Preserve newer
    Qualcomm implementations and relocate Oplus dependencies to
    vendor/qcom/sm8850-modules/oplus.
    
    Source-commit: fc30e54174d254ff7f33622a9278e4435f6718d2

--- drivers/dma-buf/heaps/qcom_system_heap.c
+++ drivers/dma-buf/heaps/qcom_system_heap.c
@@ -450,12 +450,14 @@ struct page *qcom_sys_heap_alloc_largest_available(struct dynamic_page_pool **po
 			page = dynamic_page_pool_remove(pools[i], false);
 		spin_unlock_irqrestore(&pools[i]->lock, flags);
 
+		memalloc_boost_save();
 		if (!page && movable)
 			page = qcom_movable_heap_alloc_pages(pools[i]);
 		if (!page)
 			page = alloc_pages(pools[i]->gfp_mask, pools[i]->order);
 		if (!page && alloc_reclaim && i == 1)
 			page = alloc_pages(LOW_ORDER_GFP | __GFP_RETRY_MAYFAIL, pools[i]->order);
+		memalloc_boost_restore();
 		if (!page)
 			continue;
 
