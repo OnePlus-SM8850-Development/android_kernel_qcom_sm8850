add26c1fb50b07e370fb4e555a35e822e5cabc03
chandu078 <chandudyavanapelli03@gmail.com>
2026-09-18 08:56:52 +0000

    Reapply "rproc: q6v5_pas: Disable auto boot for Canoe"
    This reverts commit 9ab92c1fcfe6285ef506c7e10e9f8f7631bb35f0.
    
    Reason for revert: Without auto_boot on the Canoe adsp/cdsp/soccp
    resources, those remoteprocs never come up.
    
    Change-Id: I0b7e67d8400d897d36787e34f7d884421347084b

--- drivers/remoteproc/qcom_q6v5_pas.c
+++ drivers/remoteproc/qcom_q6v5_pas.c
@@ -3198,6 +3198,7 @@ static const struct adsp_data canoe_adsp_resource = {
 	.uses_elf64 = true,
 	.crash_reason_stack = 660,
 	.smem_host_id = 2,
+	.auto_boot = true,
 };
 
 static const struct adsp_data canoe_cdsp_resource = {
@@ -3214,6 +3215,7 @@ static const struct adsp_data canoe_cdsp_resource = {
 	.uses_elf64 = true,
 	.crash_reason_stack = 660,
 	.smem_host_id = 5,
+	.auto_boot = true,
 };
 
 static const struct adsp_data canoe_mpss_resource = {
@@ -3247,6 +3249,7 @@ static const struct adsp_data canoe_soccp_resource = {
 	.ssr_name = "soccp",
 	.sysmon_name = "soccp",
 	.early_boot = true,
+	.auto_boot = true,
 };
 
 static const struct adsp_data ravelin_adsp_resource = {
